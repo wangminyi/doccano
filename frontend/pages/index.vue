1cafa995ade7638fe0e9bccb93a2a6677822dff1
王民艺 <305463144@qq.com>
2021-01-12 17:50:07 +0800

    删除首页

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,21 +1,30 @@
 <template>
-  <v-main>
-    <the-top-banner />
-    <feature-cards />
-    <the-bottom-banner />
-  </v-main>
+  <v-app id="inspire">
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <login-form
+              :login="authenticateUser"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import FeatureCards from '@/components/organisms/layout/FeatureCards'
-import TheTopBanner from '@/components/organisms/layout/TheTopBanner'
-import TheBottomBanner from '@/components/organisms/layout/TheBottomBanner'
+import { mapActions } from 'vuex'
+import LoginForm from '@/components/organisms/auth/LoginForm'
 
 export default {
   components: {
-    FeatureCards,
-    TheTopBanner,
-    TheBottomBanner
+    LoginForm
+  },
+
+  methods: {
+    ...mapActions('auth', ['authenticateUser'])
   }
 }
 </script>
